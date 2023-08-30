ed05cbfc8681dd32495bd2e4c80f284be715f349
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-08-30 02:45:40 -0300

    💄 styling registers list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import RegistersList from "../../components/RegistersList";
+import RegistersListFooter from "../../components/RegistersList/Footer";
+import { Box } from "@chakra-ui/react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box p="2.5rem 1.8rem 5.5rem 1.8rem" flex="1">
+      {/* <RegistersList />
+      <RegistersListFooter /> */}
+    </Box>
+  );
 };
 
 export default Dashboard;
