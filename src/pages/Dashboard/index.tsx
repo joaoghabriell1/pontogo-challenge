3c733d4176601905f452366b2c2dd2553a867e51
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-08-30 13:05:52 -0300

    💄 admin page styling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,14 @@
+import RegistersListHeader from "../../components/RegistersList/Header";
 import AllUsersRegistersList from "./AllUsersRegisters";
 import AllRegistersListFooter from "./AllRegistersListFooter";
 import { useState, Suspense } from "react";
-
 import { Box } from "@chakra-ui/react";
 const Dashboard = () => {
   const [page, setPage] = useState(1);
 
   return (
     <Box p="2.5rem 1.8rem 5.5rem 1.8rem" flex="1">
+      <RegistersListHeader />
       <Suspense fallback={<p>Loading...</p>}>
         <AllUsersRegistersList page={page} />
       </Suspense>
